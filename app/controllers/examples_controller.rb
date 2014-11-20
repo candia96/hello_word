f3b834e8abfe3f5caa7659a3eae5a7fd222d270f
candia96 <candia96@gmail.com>
2014-11-20 13:43:20 -0300

    cambios en los modelos para ver las tablas de bd

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -2,9 +2,9 @@ class ExamplesController < ApplicationController
   def comun
   	@nombre = params[:nombre_usuario]
   	@email = params[:email_usuario]
-  	@nombres= Example.all
+  	@nombres= Example.last
   	@ejemplo= Example.create({nombre: @nombre, email:@email})
   	
   end
-  
+
 end
